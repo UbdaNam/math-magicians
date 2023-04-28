d4c9c4529cb40c171207a151148e508e613a67ba
Abdurahim Miftah <abdugreat3@gmail.com>
2023-04-28 15:57:40 +0300

    Add calculator logic

--- src/components/KeyPad.js
+++ src/components/KeyPad.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import PropType from 'prop-types';
 
-const KeyPad = ({ keypad }) => (
-  <button className="key-btn" type="button">{keypad}</button>
+const KeyPad = ({ keypad, handleClick }) => (
+  <button onClick={handleClick} className="key-btn" type="button">{keypad}</button>
 );
 
 KeyPad.propTypes = {
   keypad: PropType.string.isRequired,
+  handleClick: PropType.func.isRequired,
 };
 
 export default KeyPad;
