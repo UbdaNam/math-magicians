9ef440ba05c8968fe77c026e40069f25f6c3111d
Abdurahim Miftah <abdugreat3@gmail.com>
2023-05-02 22:19:01 +0300

    Add home, calculator, and quote page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
-import './App.css';
-import Calculator from './components/Calculator';
-import Quote from './components/quote';
+import "./App.css";
+import Calculator from "./components/Calculator";
+import Home from "./components/Home";
+import Layout from "./components/Layout";
+import Quote from "./components/quote";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <div className="main-container flex-center">
-      <Calculator />
-      <Quote />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="calculator" element={<Calculator />} />
+          <Route path="quote" element={<Quote />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
