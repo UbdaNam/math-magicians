21054357a0509aedaa93cb9d72d6613a6129157d
Abdurahim .M <Abdugreat2@gmail.com>
2023-05-03 11:42:26 +0300

    Revert "Revert "Add react router""

---
+++ src/components/Home.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import style from '../styles/Home.module.css';
+
+export default function Home() {
+  return (
+    <section className={style['intro-container']}>
+      <h3>Welcome to our page!</h3>
+      <div className={style['intro-content']}>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Est
+          distinctio rerum itaque, facere at impedit vitae, necessitatibus
+          ullam, natus voluptatum illo non ducimus deserunt dicta maxime rem
+          corrupti aspernatur molestias a iure labore temporibus aut quasi!
+          Alias minus iure accusantium quisquam sint incidunt cupiditate magni
+          iusto veniam facilis voluptatibus beatae necessitatibus voluptas
+          illum, enim dolor fuga repudiandae corrupti quas at doloremque. Vitae
+          nesciunt blanditiis nam praesentium a, facilis pariatur voluptate!
+        </p>
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magni
+          exercitationem voluptate omnis obcaecati rerum vero, at tempora
+          possimus illo, odio molestias odit ad maxime sed molestiae, quidem ex
+          harum qui. Vitae nulla mollitia ullam voluptas, voluptatem dignissimos
+          eos iste ducimus dolorem suscipit omnis? Repellendus laboriosam
+          sapiente alias enim autem fugit inventore, reiciendis earum dolores
+          excepturi expedita velit harum porro, nam ratione amet ab fuga, a
+          magnam possimus quae tempora. Amet, nostrum laudantium? Quos iure fuga
+          debitis? Amet minus exercitationem quas.
+        </p>
+      </div>
+    </section>
+  );
+}
